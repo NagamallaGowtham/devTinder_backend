4352d166fd54b94fd40e5d82297ebe99df406ba0
Gowtham <nagamallagowtham@gmail.com>
2026-03-07 19:40:47 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,21 +2,15 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/user", (req, res) => {
-//     res.send("Hello Ramaaaaaa");
-// });
-
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params);
-    res.send({firstname: "Rama", lastname: "Sita"});
-});
-
-app.post("/user", (req, res) => {
-    res.send("succesfullly posted");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("deleted the user");
+app.use("/user", [(req, res, next) => {
+    // res.send("Handler 1");
+    next();
+}, (req, res, next) => {
+    // res.send("Handler 2");
+    next();
+}], (req, res, next) => {
+    next();
+    res.send("Handler 3");
 });
 
 app.listen(3000, () => {
