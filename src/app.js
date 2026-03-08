9d8bac8de8dd9e493e9096bf3ad2739c5fc05e82
Gowtham <nagamallagowtham@gmail.com>
2026-03-08 12:07:36 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ const express = require("express");
 
 const app = express();
 
-const { auth } = require("./middlewares/auth");
-
-// app.use("/admin", auth);
-
 app.get("/user", (req, res) => {
-    res.send("user request");
-})
-
-app.get("/admin/deleteUser", auth, (req, res) => {
-    res.send("deleted user");
+    try {
+        throw Error("error");
+        res.send("user input");
+    }
+    catch(e) {
+        res.status(500).send("Something went wrong!");
+    }
 });
-app.get("/admin/loginUser", auth, (req, res) => {
-    res.send("login successfully");
-});
 
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong!");
+    }
+});
 
 app.listen(3000, () => {
     console.log("Server started running successfully!");
