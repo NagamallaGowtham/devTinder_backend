d3cca6b1c20f7c3270797d1fa03650dc144bf09b
Gowtham <nagamallagowtham@gmail.com>
2026-03-08 09:07:56 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -2,16 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", [(req, res, next) => {
-    // res.send("Handler 1");
-    next();
-}, (req, res, next) => {
-    // res.send("Handler 2");
-    next();
-}], (req, res, next) => {
-    next();
-    res.send("Handler 3");
+const { auth } = require("./middlewares/auth");
+
+// app.use("/admin", auth);
+
+app.get("/user", (req, res) => {
+    res.send("user request");
+})
+
+app.get("/admin/deleteUser", auth, (req, res) => {
+    res.send("deleted user");
 });
+app.get("/admin/loginUser", auth, (req, res) => {
+    res.send("login successfully");
+});
+
 
 app.listen(3000, () => {
     console.log("Server started running successfully!");
---
+++ src/middlewares/auth.js
@@ -0,0 +1,12 @@
+const auth = (req, res, next) => {
+    const token = "xyz";
+    const isAuthorized = token === "xyz";
+
+    if (!isAuthorized) {
+        res.status(401).send("Unauthorized");
+    } else {
+        next();
+    }
+};
+
+module.exports = {auth}
