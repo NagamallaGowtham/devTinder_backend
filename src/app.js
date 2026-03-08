e2e68d6dc0daaab19eccdf011c3c5a5bfe1c1ae6
Gowtham <nagamallagowtham@gmail.com>
2026-03-08 22:32:44 +0530

    init schema

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const {connectDB} = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+    const user = new User(req.body);
+
+    try {
+        await user.save();
+        res.send("user saved successfully");
+    } catch(e) {
+        res.status(400).send("Something went wrong");
+    }
+})
 
 connectDB()
     .then(() => {
