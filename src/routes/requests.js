f552323be616937839bde6473db1b32c349fcd15
Gowtham <nagamallagowtham@gmail.com>
2026-03-28 23:45:47 +0530

    send request api

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const { authUser } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendRequest", authUser, async (req, res) => {
+requestRouter.post("/request/send/:status/:userId", authUser, async (req, res) => {
     try {
-        const user = req.user;
+        const fromUserId = req.user._id;
+        const toUserId = req.params.userId;
+        const status = req.params.status;
 
-        res.send(user.firstName + " sent you connection request");
-    } catch(e) {
-        res.status(400).send("Error: " + e.message);
+        const allowedStatus = ["interested", "ignored"];
+        const isStatusAllowed = allowedStatus.includes(status);
+        if (!isStatusAllowed) {
+            throw new Error("Status type is not valid!");
+        }
+
+        const user = await User.findById(toUserId);
+        if (!user) {
+            throw new Error("User doesn't exist");
+        }
+
+        const shouldRequestBeSent = await ConnectionRequest.findOne({
+            $or: [
+                {fromUserId, toUserId},
+                {fromUserId: toUserId, toUserId: fromUserId}
+            ]
+        });
+        if (shouldRequestBeSent) {
+            throw new Error("Request can't be sent");
+        }
+
+        const request = await ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
+
+        await request.save();
+        res.json({status: true, data: request});
+    } catch (err) {
+        res.status(400).json({status: false, message: err.message});
     }
 });
 
